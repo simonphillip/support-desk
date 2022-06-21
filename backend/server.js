cbd0612e588abc9d6599385d33b6c91c96f2de89
simonphillip <simonphillip@live.com>
2022-06-21 15:15:54 +1000

    Created backend authorization
    Created backend authorization routes for user registration and login.

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,34 @@
-console.log('Server...')
+//import express
+const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware');
+const {connectDB} = require('./config/db');
+const PORT = process.env.PORT || 5000;
+
+// Connect to database
+connectDB();
+
+//initialize app variable
+const app = express();
+
+//middleware that allows your requests to return json
+app.use(express.json());
+
+app.use(express.urlencoded({extended: false}));
+
+//get request
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: "Welcome to the Support Desk API"
+    });
+})
+
+//Routes
+//Hook up user route file to api endpoint
+app.use('/api/users', require('./routes/userRoutes'));
+
+app.use(errorHandler);
+
+//listen in to port
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
