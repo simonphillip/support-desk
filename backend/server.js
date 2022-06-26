3f80a488c206feef7aecc69373665bade13b56a1
simonphillip <simonphillip@live.com>
2022-06-26 10:20:25 +1000

    prep for heroku push
    added in logic to server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 //import express
+const path = require('path');
 const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config()
@@ -17,18 +18,28 @@ app.use(express.json());
 
 app.use(express.urlencoded({extended: false}));
 
-//get request
-app.get('/', (req, res) => {
-    res.status(200).json({
-        message: "Welcome to the Support Desk API"
-    });
-})
+
 
 //Routes
 //Hook up user route file to api endpoint
 app.use('/api/users', require('./routes/userRoutes'));
 //Hook up ticket route file to api endpoint
 app.use('/api/tickets', require('./routes/ticketRoutes'));
+
+//Serve frontend
+if (process.env.NODE_ENV === 'production') {
+    // Set build folder as static
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+    app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'));
+} else {
+    //get request
+    app.get('/', (req, res) => {
+    res.status(200).json({
+        message: "Welcome to the Support Desk API"
+    });
+})
+}
 
 app.use(errorHandler);
 
